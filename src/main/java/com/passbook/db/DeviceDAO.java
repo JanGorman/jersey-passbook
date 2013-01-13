081f7f3194aa44f28e3110f7d1139a86efd302d3
Jan Gorman <gorman.jan@gmail.com>
2013-01-13 17:31:51 +0100

    register method

--- src/main/java/com/passbook/db/DeviceDAO.java
+++ src/main/java/com/passbook/db/DeviceDAO.java
@@ -1,5 +1,6 @@
 package com.passbook.db;
 
+import com.google.common.base.Optional;
 import com.passbook.core.Device;
 import com.yammer.dropwizard.hibernate.AbstractDAO;
 import org.hibernate.SessionFactory;
@@ -8,6 +9,10 @@ public class DeviceDAO extends AbstractDAO<Device> {
 
     public DeviceDAO(SessionFactory sessionFactory) {
         super(sessionFactory);
+    }
+
+    public Optional<Device> findByPassTypeIdentifierAndSerialNumber(String passTypeIdentifier, String serialNumber) {
+        return Optional.fromNullable(null);
     }
 
 }
--- src/main/java/com/passbook/resources/PassbookDevicesResource.java
+++ src/main/java/com/passbook/resources/PassbookDevicesResource.java
@@ -1,10 +1,37 @@
 package com.passbook.resources;
 
+import com.google.common.base.Optional;
+import com.passbook.core.Device;
+import com.passbook.db.DeviceDAO;
+
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/v1/devices")
 @Produces(MediaType.APPLICATION_JSON)
 public class PassbookDevicesResource {
+
+    private final DeviceDAO deviceDAO;
+
+    public PassbookDevicesResource(DeviceDAO deviceDAO) {
+        this.deviceDAO = deviceDAO;
+    }
+
+    @POST
+    @Path("{deviceLibraryIdentifier}/registrations/{passTypeIdentifier}/{serialNumber}")
+    public Response registerDevice(@PathParam("deviceLibraryIdentifier") String deviceLibraryIdentifier,
+                                   @PathParam("passTypeIdentifier") String passTypeIdentifier,
+                                   @PathParam("serialNumber") String serialNumber) {
+        Optional<Device> device = deviceDAO.findByPassTypeIdentifierAndSerialNumber(passTypeIdentifier, serialNumber);
+        if (!device.isPresent()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return null;
+    }
+
 }
